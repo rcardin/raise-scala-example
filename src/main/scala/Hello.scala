3d0d29b6d27661c3d2491af492b92866c9e37e9c
rcardin <riccardo.cardin@gmail.com>
2024-04-08 13:00:21 +0200

    First draft of Raise and fold function with example

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,11 +1,40 @@
-trait Raise[Error]:
+trait Raise[-Error]:
   def raise(e: Error): Nothing
 
-def fold[A, B, Error: Raise](
-    block: () => B,
+class DefaultRaise extends Raise[Any]:
+  def raise(e: Any): Nothing = throw Raised(e)
+
+class Raised[Error](val original: Error) extends Throwable
+
+def fold[A, B, Error](
+    block: Raise[Error] ?=> () => A,
     catchBlock: (throwable: Throwable) => B,
     recover: (error: Error) => B,
     transform: (value: A) => B
-): B = ???
+): B =
+  given raise: Raise[Error] = new DefaultRaise
+  try transform(block())
+  catch
+    case e: Raised[Error] => recover(e.original)
+    case e: Throwable     => catchBlock(e)
+end fold
 
-@main def helloWorld = println("Hello, world")
+sealed trait Error
+case object MyError extends Error
+
+@main def helloWorld(): Unit =
+  val result: String = fold[Int, String, Error](
+    () => 42,
+    throwable => "Error: " + throwable.getMessage,
+    error => "Error: " + error,
+    value => "Value: " + value
+  )
+  println(result)
+
+//  val result2: String = fold[Int, String, Error](
+//    () => raise(MyError),
+//    throwable => "Error: " + throwable.getMessage,
+//    error => "Error: " + error,
+//    value => "Value: " + value
+//  )
+//  println(result)
