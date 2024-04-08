c2e26ad3151e98679fa8820ccf2425b2291820ad
rcardin <riccardo.cardin@gmail.com>
2024-04-08 10:21:33 +0200

    First draft of Raise and fold function

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,1 +1,11 @@
+trait Raise[Error]:
+  def raise(e: Error): Nothing
+
+def fold[A, B, Error: Raise](
+    block: () => B,
+    catchBlock: (throwable: Throwable) => B,
+    recover: (error: Error) => B,
+    transform: (value: A) => B
+): B = ???
+
 @main def helloWorld = println("Hello, world")
